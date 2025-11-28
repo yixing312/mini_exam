f4f151a0f21c2ee6ec09980b9758d79cb4d0f9c1
yixing <yanjizhou312@163.com>
2025-11-28 17:03:09 +0800

    add: 轨迹运行

--- examples/motion.rs
+++ examples/motion.rs
@@ -1,15 +1,25 @@
 use libjaka::JakaMini2;
-use robot_behavior::{MotionType, Pose, behavior::*};
 use nalgebra as na;
+use robot_behavior::{MotionType, Pose, behavior::*};
 
 fn main() -> anyhow::Result<()> {
     let mut robot = JakaMini2::new("10.5.5.100");
 
-    robot.move_joint(&[0.;6])?;
-    robot.move_to(MotionType::Joint([0.;6]))?;
+    robot.move_joint(&[0.; 6])?;
+    robot.move_to(MotionType::Joint([0.; 6]))?;
 
     robot.move_cartesian(&Pose::Quat(na::Isometry3::identity()))?;
     robot.move_to(MotionType::Cartesian(Pose::Quat(na::Isometry3::identity())))?;
+
+    // 使用 `xx.json` 文件中的轨迹进行运动
+    // 文件内要求轨迹形式为：
+    // [
+    //   { "Joint": [1.0, 2.0, 3.0] },
+    //   { "Joint": [1.0, 2.0, 3.0] },
+    //   { "JointVel": [0.1, 0.2, 0.3] },
+    //   { "Cartesian": {"Euler": [[0.0, 0.0, 0.5], [0.0, 0.0, 0.0]] }}
+    // ]
+    robot.move_traj_from_file("xx.json")?;
 
     Ok(())
 }
@@ -25,7 +35,7 @@ mod tests {
         robot.move_joint(&JakaMini2::JOINT_DEFAULT)?;
         Ok(())
     }
-    
+
     #[test]
     fn move_to_packed() -> RobotResult<()> {
         let mut robot = JakaMini2::new("10.5.5.100");
